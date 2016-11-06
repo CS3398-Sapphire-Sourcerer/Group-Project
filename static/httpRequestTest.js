8d154c373c1125888e6847f4ee3b38184bbe5740
Casey Sledge <cjs221@txstate.edu>
2016-11-06 17:27:07 -0600

    Added socketIO to included files. Rewrote app.html with socketIO and comments. Rewrote httpRequestTest.js in support of app.html.

--- static/httpRequestTest.js
+++ static/httpRequestTest.js
@@ -2,37 +2,18 @@
  * Created by Casey on 10/27/2016.
  */
 
-function fireLocationUpdate(position, userID) {
-    console.log(position);
-    console.log(userID);
-    let coords = position.coords;
-    let lat = position.coords.latitude;
-    let long = position.coords.longitude;
-
-    console.log("fireLocationUpdate");
-
-    var myRequest = new XMLHttpRequest(),
-        method = "POST",
-        //user = 1,
-        url = "updatePos",
-        urlString = url + "/" + userID + "/" + lat + "/" + long;
-
-    console.log(userID);
-    myRequest.open(method, urlString, true);
-    myRequest.onreadystatechange = function () {
-        if (myRequest.readyState == XMLHttpRequest.DONE && myRequest.status == 200) {
-            console.log(myRequest.responseText);
-        }
-    }
-
-    myRequest.send();
+function sendLocation(locationObj) {
+    console.log("Getting to sendLocation");
+    socket.emit('changeLocation', locationObj);
 }
-
-function geolocateUser(userID) {
-    console.log("A field of lies");
+0
+function geolocateUser(userID, locationObj) {
     console.log("geolocateUser");
     console.log("userID : ", userID);
     navigator.geolocation.getCurrentPosition(function (position) {
-        fireLocationUpdate(position, userID);
+        locationObj.uid = userID;
+        locationObj.latitude = position.coords.latitude;
+        locationObj.longitude = position.coords.longitude;
+        sendLocation(locationObj)
     });
 }
