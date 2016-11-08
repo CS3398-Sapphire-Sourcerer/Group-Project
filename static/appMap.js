f8a49f84829ce677d9655db41e823dd9c13fddca
Jeremiah <jeb231@txstate.edu>
2016-11-08 01:22:37 -0600

    add the border around Derrick

--- static/appMap.js
+++ static/appMap.js
@@ -2,6 +2,10 @@
  * Created by Casey on 11/3/2016.
  */
 //this holds the home page map that is is found in index.html
+//colors (hex):
+//black:  #000000
+//gold:   #ffd700
+//maroon: #800000
 
 function appMap() {
     var mapOptions = {
@@ -13,3 +17,20 @@ function appMap() {
     };
     var map = new google.maps.Map(document.getElementById("map"), mapOptions);
 }
+
+var derrickCoordinates = [
+    { lat:29.889579, lng:-97.942417 },
+    { lat:29.888962, lng:-97.942326 },
+    { lat:29.888970, lng:-97.942107 },
+    { lat:29.889264, lng:-97.942159 },
+    { lat:29.889313, lng:-97.941819 },
+    { lat:29.889646, lng:-97.941899 },
+    { lat:29.889579, lng:-97.942417 }
+];
+
+var Derrick = new google.maps.Polygon({
+    paths: derrickCoordinates,
+    strokeColor: '#800000' //color: maroon
+});
+
+Derrick.setMap(map);
