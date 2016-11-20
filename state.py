22292a0a8851405b115b7aea0607389e627154ca
Casey Sledge <cjs221@txstate.edu>
2016-11-20 17:35:26 -0600

    Added state.py (state generation and maintenance), added state initialization in views

---
+++ state.py
@@ -0,0 +1,32 @@
+from init import app
+def gameState():
+    req = None
+    state = dict(buildings=["Derrick", "Comal"])
+
+    # add all of the buildings from the DB to the state object
+    app.logger.info("Made it into the function")
+    app.logger.info(state)
+    req = yield
+    app.logger.info("Made it past")
+    app.logger.info(req)
+
+    while req is not None:
+        app.logger.info("Made it into the loop")
+        if req['type'] == "new":
+            app.logger.info("type is new")
+            app.logger.info(state)
+            yield state
+            req = yield
+
+game = gameState()
+
+def initializeGame():
+    next(game)
+
+def requestState(req):
+    pass
+
+def updateState(req):
+    pass
+
+
--- views.py
+++ views.py
@@ -9,6 +9,7 @@ import questionLogic
 import majorIDs
 from init import app, datab
 from flask import request
+from state import initializeGame
 
 
 @app.before_request
@@ -22,6 +23,8 @@ def setup_user():
             del flask.session['auth_user']
         # save the user in `flask.g`, which is a set of globals for this request
         flask.g.user = user
+    initializeGame()
+
 
 # this is the function that will show the user the home.html page
 @app.route('/', methods=['GET'])
