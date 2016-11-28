98254ce0addcf42e9c32a2ee8f9e6fe38bb73cb2
Jeremiah <jeb231@txstate.edu>
2016-11-27 20:09:04 -0600

    added all the building and their coordinates

---
+++ major_list_enum.py
@@ -0,0 +1,39 @@
+from enum import Enum
+#list of majors at Texas State associated with a number
+#This Enum will be used to classify the buildings ans question to respective majors
+class Majors(Enum):
+    Accounting = 1
+    Agriculture = 2
+    Anthropology = 3
+    Art = 4
+    History = 5
+    Biology = 6
+    Chemistry = 7
+    Computer_Science = 8
+    Criminal_Justice= 9
+    Dance = 10
+    Economics = 11
+    Electrical_Engineering = 12
+    English = 13
+    Exercise_and_Sports_Science = 14
+    Foreign_Languages = 15
+    Geography = 16
+    History = 17
+    Industrial_Engineering = 18
+    International_Relations = 19
+    Journalism_and_Mass_Communication = 20
+    Management = 21
+    Manufacturing_Engineering = 22
+    Marketing = 23
+    Mass_Communication = 24
+    Mathematics = 25
+    Music = 26
+    Nursing = 27
+    Nutrition_and_Foods = 28
+    Philosophy = 29
+    Photography = 30
+    Physics = 31
+    Political_Science = 32
+    Psychology = 33
+    Radiation_Therapy = 34
+    Sociology = 35
