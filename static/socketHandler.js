3c7b773de69d2b565b7cd860bc42ff8ff6b12cff
Casey Sledge <cjs221@txstate.edu>
2016-11-30 16:20:51 -0600

    socketHandler.js : Correcting continuous assignment error

--- static/socketHandler.js
+++ static/socketHandler.js
@@ -25,6 +25,7 @@ function updateLocation() {
     if (locDifference.latitude < .00001 && locDifference.longitude < .00001) {
         console.log("Difference miniscule.");
         return;
+    }
     else {
         console.log("Delta significant");
         let locObj = checkLocation();
@@ -34,19 +35,21 @@ function updateLocation() {
             }
         */
 
+        // commit changes?
+
         if (locObj.building == "") {
             console.log("Not in a building");
-            locationObj.continuous == false;
-            locationObj.lastBuilding == null;
+            locationObj.continuous = false;
+            locationObj.lastBuilding = null;
         }
         else if (locObj.building == locationObj.lastBuilding) {
             console.log("In the same building");
-            locationObj.continuous == 1;
+            locationObj.continuous = 1;
         }
         else {
             console.log("Entered a building");
-            locationObj.continuous == 0;
-            locationObj.lastBuilding == locObj.building;
+            locationObj.continuous = 0;
+            locationObj.lastBuilding = locObj.building;
         }
         
         lastLocation = locationObj;
@@ -55,19 +58,28 @@ function updateLocation() {
         emitObject.building = locObj.building;
 
         console.log("emitObject : ", emitObject);
-        
+        console.log(".building : ", emitObject.building);
+        console.log(".latitude : ", emitObject.latitude);
+        console.log(".longitude : ", emitObject.longitude);
+        console.log(".continuous : ", emitObject.continuous);
+        console.groupEnd();   
         socket.emit("changeLocation", emitObject);
     }
+}
+
+//force save
+
+function checkLocation() {
+    console.group("checkLocation() [socketHandler.js]");
+    return {"building":checkBuildingBounds(locationObj)};
     console.groupEnd();
 }
 
-function checkLocation() {
-    return {"building":checkBuildingBounds(locationObj)};
-}
-
 socket.on('enterBuilding', inBuilding);
+//This socket should not be used, but has not yet been removed.
 function inBuilding(obj) {
     console.group("inBuilding(obj) [socketHandler.js][listener]");
+    console.log("This message should not be playing, find out what is emitting \'enterBuilding\'.")
     if (obj.building != -1) {
         console.log("Good building ID");
 
@@ -110,6 +122,13 @@ function alertQuestionBar(obj) {
     console.groupEnd();
 }
 
+socket.on('noChangeEvent', noChange);
+function noChange(obj) {
+    console.group("noChangeEvent(obj) [socketHandler.js][listener]");
+    console.log("User is not in building, no change event playing");
+    console.groupEnd();
+}
+
 
 socket.on('result', writeResult);
 function writeResult(result) {
@@ -130,9 +149,8 @@ function emitAnswer(buttonObj) {
     };
     //var userAnswerId = buttonObj.value;
     //var questionId = document.getElementById("q_text_1").value;
-
-    socket.emit("answer", submit);
     console.groupEnd();
+    socket.emit("answer", submit);
 }
 
 function testButtonValue(buttonObj) {
