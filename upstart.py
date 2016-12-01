5239f676a3f14a9051f8150c95980ee11e11fd97
Jeremiah <jeb231@txstate.edu>
2016-12-01 15:40:50 -0600

    fixed re-population error for teams...

--- upstart.py
+++ upstart.py
@@ -5,23 +5,25 @@ from init import datab
 
 
 def populate_teams():
-    teams = models.Team()
-    teams1 = models.Team()
-    teams2 = models.Team()
-    teams3 = models.Team()
+    query = models.Teams.query.first()
+    if query is None:
+        teams = models.Team()
+        teams1 = models.Team()
+        teams2 = models.Team()
+        teams3 = models.Team()
 
-    teams.name = 'Neutral'
-    teams.score = 0
-    datab.session.add(teams)
-    teams1.name = 'Maroon'
-    teams1.score = 0
-    datab.session.add(teams1)
-    teams2.name = 'Black'
-    teams2.score = 0
-    datab.session.add(teams2)
-    teams3.name = 'Gold'
-    teams3.score = 0
-    datab.session.add(teams3)
-    datab.session.commit()
+        teams.name = 'Neutral'
+        teams.score = 0
+        datab.session.add(teams)
+        teams1.name = 'Maroon'
+        teams1.score = 0
+        datab.session.add(teams1)
+        teams2.name = 'Black'
+        teams2.score = 0
+        datab.session.add(teams2)
+        teams3.name = 'Gold'
+        teams3.score = 0
+        datab.session.add(teams3)
+        datab.session.commit()
 
     return
